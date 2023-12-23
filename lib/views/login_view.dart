8363a4057b18454b70cc67c6ef5b84060e4bfade
Alperen Göyce <goycea@gmail.com>
2023-12-23 22:49:57 +0300

    login view commit

---
+++ lib/views/login_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LogiView extends StatefulWidget {
+  const LogiView({Key? key}) : super(key: key);
+
+  @override
+  State<LogiView> createState() => _LogiViewState();
+}
+
+class _LogiViewState extends State<LogiView> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(body: Column(children: [],),));
+  }
+}

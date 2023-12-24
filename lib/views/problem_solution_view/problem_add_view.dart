5235c1cea177aad67b47555c94c8b74e2caadc22
muhamparlak <muhammetprlk72@gmail.com>
2023-12-24 06:14:21 +0300

    add problem view

---
+++ lib/views/problem_solution_view/problem_add_view.dart
@@ -0,0 +1,108 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class AddProblemView extends StatefulWidget {
+  const AddProblemView({Key? key}) : super(key: key);
+
+  @override
+  State<AddProblemView> createState() => _AddProblemViewState();
+}
+
+class _AddProblemViewState extends State<AddProblemView> {
+  File? img = null;
+  Future pickImageGalery(ImageSource source) async {
+    final image = await ImagePicker().pickImage(source: source);
+    if (image == null) return null;
+
+    final imageTemporary = File(image.path);
+    img = imageTemporary;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 60.0),
+            child: Center(
+              child: Container(
+                width: 200,
+                height: 150,
+                /*decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(50.0)),*/
+                child: img != null ? Image.file(img!) : Container(),
+              ),
+            ),
+          ),
+          Padding(
+            //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Title',
+                  hintText: ''),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 15.0, right: 15.0, top: 15, bottom: 0),
+            //padding: EdgeInsets.symmetric(horizontal: 15),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Description',
+                  hintText: ''),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 15.0, right: 15.0, top: 15, bottom: 15),
+            //padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(48, 77, 48, 1),
+                  borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                onPressed: () {
+                  pickImageGalery(ImageSource.gallery);
+                },
+                child: Text(
+                  'Add Photo',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 50,
+            width: 250,
+            decoration: BoxDecoration(
+                color: Color.fromRGBO(48, 77, 48, 1),
+                borderRadius: BorderRadius.circular(20)),
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                'Create Problem',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 130,
+          ),
+        ],
+      ),
+    ));
+  }
+}
